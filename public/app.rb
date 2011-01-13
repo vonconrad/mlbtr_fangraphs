d7d206aa0f247b15f9c424b3e9304e8f8e1f81c3
vonconrad <sebastian@e-channel.com.au>
2011-01-13 21:39:57 +1030

    Add route functionality

--- public/app.rb
+++ public/app.rb
@@ -1,5 +1,13 @@
+c = Cabinet::Local.new(File.join(File.dirname(__FILE__), '..', 'tmp'))
+f = 'feed.xml'
+
 get '/' do
+  c.put(f, XML.new(open('http://feeds.feedburner.com/MlbTradeRumors')).transform) if c.modified(f) + 600 <= Time.now
+
+  content_type 'text/xml', :charset => 'utf-8'
+  c.get(f)
 end
 
 get '/reload' do
+  c.put(f, XML.new(open('http://feeds.feedburner.com/MlbTradeRumors')).transform)
 end
